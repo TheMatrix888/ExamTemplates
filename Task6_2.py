6da7155210f03081c22c33460da68c7e9e03cb49
unknown <TheMatrix88888@gmail.com>
2024-04-15 17:38:29 +0300

    Added Task 6_2

---
+++ Task6_2.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+m = 20
+d = 5
+
+up()
+for i in range(10):
+    forward(5*m)
+    dot(d)
+    right(90)
+    forward(3*m)
+    dot(d)
+    right(90)
+    forward(2*m)
+    dot(d)
+    right(90)
+
+input()
